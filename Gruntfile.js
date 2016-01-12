6fb4894704135835d3a1a85f75090a953fe3f0d4
christopheboisier <christophe.boisier@live.fr>
2016-01-12 16:18:18 +0100

    Add CNAME file

--- Gruntfile.js
+++ Gruntfile.js
@@ -336,7 +336,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'CNAME'
           ]
         }, {
           expand: true,
